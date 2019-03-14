e71e5de5eb9d38e010562417c8713515de82dea9
leonardo ferreira <leovsferreira@gmail.com>
2019-03-14 18:36:25 -0300

    added crud to kids controller

--- app/controllers/kids_controller.rb
+++ app/controllers/kids_controller.rb
@@ -1,4 +1,13 @@
 class KidsController < ApplicationController
+  before_action :set_kid, only: %i[show edit update destroy]
+
+  def index
+    @kids = Kid.all
+  end
+
+  def show
+  end
+
   def new
     @kid = Kid.new
   end
@@ -6,16 +15,38 @@ class KidsController < ApplicationController
   def create
     @kid = Kid.new(kid_params)
     if @kid.save
-      flash[:notice] = 'You added your kid successfully.'
+      flash[:notice] = "You added #{@kid.name} profile successfully."
       redirect_to root_path
     else
       render :new
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @kid.update(kid_params)
+      flash[:notice] = "You updated #{@kid.name} profile successfully."
+      redirect_to kid_path(@kid)
+    else
+      render :show
+    end
+  end
+
+  def destroy
+    @kid.destroy
+    flash[:notice] = "You deleted #{@kid.name} profile successfully."
+    redirect_to kids_path
+  end
+
   private
 
   def kid_params
     params.require(:kid).permit(:name, :gender, :born_at)
+  end
+
+  def set_kid
+    @kid = Kid.find(params[:id])
   end
 end
