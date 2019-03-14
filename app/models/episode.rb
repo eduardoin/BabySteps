b50062d4725102c80d5b79550d6f13b7166e85f9
leonardo ferreira <leovsferreira@gmail.com>
2019-03-14 17:15:01 -0300

    Add creation to child

--- app/models/episode.rb
+++ app/models/episode.rb
@@ -2,7 +2,5 @@ class Episode < ApplicationRecord
   belongs_to :kid
   validates :tracked_at, :data, presence: true
 
-  self.abstract_class = true
-
   extend DataAttribute
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :tutelages, dependent: :destroy
+  has_many :kids, through: :tutelages
 
   validates :email, presence: true, uniqueness: true
   validates :username, presence: true, uniqueness: true
