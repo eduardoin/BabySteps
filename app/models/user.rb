4ad03e13bfd77e0ca2b40cb46e97d641250cee0f
svetlana berrios <sveta.berrios@gmail.com>
2019-03-15 12:30:53 -0300

    modified devise files for user to login with username and password

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,19 @@ class User < ApplicationRecord
 
   validates :email, presence: true, uniqueness: true
   validates :username, presence: true, uniqueness: true
+
+  attr_writer :login
+
+  def login
+    @login || self.username || self.email
+  end
+
+  def self.find_for_database_authentication(warden_conditions)
+      conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions.to_hash).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+      elsif conditions.has_key?(:username) || conditions.has_key?(:email)
+        where(conditions.to_hash).first
+    end
+  end
 end
