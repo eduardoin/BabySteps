fe9463fe0a165b4ac1308aa1d45e1a59db6a52c1
svetlana berrios <sveta.berrios@gmail.com>
2019-03-20 11:12:22 -0300

    finalized the full_log changes

--- app/models/diaper_changing.rb
+++ app/models/diaper_changing.rb
@@ -2,4 +2,16 @@ class DiaperChanging < Episode
   episode_attr :rash, :pee, :poop, :diarrhea, :blood
   boolean_attr :rash, :pee, :poop, :diarrhea, :blood
 
+  def present_log
+    last_key = data.keys.last
+    data.map do |key, value|
+      if value == "1"
+        <<-HTML
+          <li class="log_card-subtitle" style="display: inline;">
+            #{key}#{',' unless last_key == key}
+          </li>
+        HTML
+      end
+    end.join.html_safe
+  end
 end
